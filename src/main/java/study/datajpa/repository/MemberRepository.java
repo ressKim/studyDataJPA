b6abd262797d5c851b9eb3045686069d40182479
hyukjun kim <sh21dlat@gmail.com>
2021-02-24 07:41:48 +0900

    @Query, repository 에 쿼리 정의하기 - 컴파일시점에서 문법자체 오류도 확인해 준다. 복잡할때는 @Query 로 풀면된다

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -9,11 +9,15 @@ import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    List<Member> findByUsernameAndAgeGreaterThan(String userName, int age);
+    List<Member> findByUsernameAndAgeGreaterThan(String userName, int age);//길어지면 너무 길어진다
 
     List<Member> findTop3HelloBy();
 
 //    @Query(name = "Member.findByUsername")
     List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")//복잡한걸 쿼리로 바로 표현할 수 있다.
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
 
 }
