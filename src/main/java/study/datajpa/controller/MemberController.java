eab989f2633546b750a6e17f3980f482dc3945f5
ressKim <sh21dlat@gmail.com>
2021-02-27 22:18:01 +0900

    페이징과 정렬 - 주의점 - 엔티티를 그대로 넘기지 말자 , DTO 내부에서 entity 를 불러와서 dto 를 만드는것은 괜찮으니 그런식으로 dto를 만들어서 넘기자

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,10 +1,14 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -27,9 +31,16 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")//page 설정 방법 - 전체 설정보다 개인별 설정이 우선시된다.
+    public Page<MemberDto> list(@PageableDefault(size = 5, sort = "username") Pageable pageable) {
+        return memberRepository.findAll(pageable).map(MemberDto::new);
+    }
+
     @PostConstruct
-    public void init(){
-        memberRepository.save(new Member("userA"));
+    public void init() {
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 
 }
--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
         query= "select m from Member m where m.username = :username"
 )
 @NamedEntityGraph(name = "Member.all", attributeNodes = @NamedAttributeNode("team"))
-public class Member extends BaseEntity {
+public class Member {
 
     @Id
     @GeneratedValue
